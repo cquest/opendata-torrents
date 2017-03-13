b8508bf480b291aec4fdd221d9f6bca7dbbbc138
Christian Quest <cquest@openstreetmap.fr>
2017-03-13 19:13:59 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -3,18 +3,20 @@
 # get TRANSMISSION_AUTH
 . config.sh
 
+
+# Météo-France Arpege
 cd /var/www/html/torrents/meteo-france/arpege/
-for t in $( find ./ -name '*torrent' -cmin +900 )
+for t in $( find ./ -name '*torrent' -cmin +720 )
 do
   # delete torrent file from web site
   rm $t
 done
-cd -
-./rss.sh torrents/meteo-france/arpege > /var/www/html/torrents/meteo-france/arpege/rss.xml
+cd - > /dev/null
+./rss.sh torrents/meteo-france/arpege > /var/www/html/torrents/meteo-france/arpege/rss.xml "Météo-France" rss-arpege.xml
 
 
 cd /var/lib/transmission-daemon/info/torrents
-for t in $( find ./ -name '*torrent' -cmin +1440 )
+for t in $( find ./ -name 'mf-*torrent' -cmin +1440 )
 do
   # get original file name
   torrent=$( echo $t | sed 's!.*\(mf-.*grib2\).*!\1!' )
@@ -23,9 +25,9 @@ do
   num=$( transmission-remote -n "$TRANSMISSION_AUTH" -l | grep "$torrent" | grep -o '^ *[0-9]*' )
   # remove torrent from transmission-daemon
   transmission-remote -n "$TRANSMISSION_AUTH" -t $num -r
-
-  # delete data file
-  rm /var/lib/transmission-daemon/downloads/$t
 done
+
+# delete data files
+find /var/lib/transmission-daemon/downloads -name "*.grib2" -cmin +1440 -delete
 
 
