3b1bb4be89e1a0c8d1c7f1eceba6947c64034197
Christian Quest <cquest@openstreetmap.fr>
2017-03-13 19:15:42 +0100

    paramétrage source

--- rss.sh
+++ rss.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cat rss.xml
+cat $3
 cd /var/www/html/$1
 
 for t in *.torrent
 do
+  d=$( stat $t -c %y | egrep -o '^.{16}' )
+  d=$( date -d "$d UTC" -R )
   echo "<item>"
-  echo "  <title>$t</title><guid isPermaLink='true'>http://212.47.238.202/$1/$t</guid>"
-  echo "  <pubDate>Mon, 03 Oct 2016 11:35:38 +0200</pubDate>"
+  echo "  <title>$t</title>"
+  echo "  <pubDate>$d</pubDate>"
   echo "  <category>data</category><link>http://212.47.238.202/$1/$t</link>"
-  echo "  <description>$t</description><comments>Source: Météo-France, Licence Ouverte</comments>"
+  echo "  <description>$t</description><comments>Source: $2, Licence Ouverte</comments>"
   echo "  <enclosure url='http://212.47.238.202/$1/$t' type='application/x-bittorrent' />"
   echo "</item>"
 done
